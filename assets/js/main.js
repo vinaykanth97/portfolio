c38f12f0c83126acfdcef39cf3bbb4fefe5df253
vinaykanth97 <vinaykennedy@gmail.com>
2024-03-19 00:23:14 +0530

    added some scrollto scripts

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+gsap.registerPlugin(ScrollToPlugin)
 let reviewSwiper = new Swiper(".swiper-container", {
     spaceBetween: 30,
     slidesPerView: 3,
@@ -9,3 +10,21 @@ let reviewSwiper = new Swiper(".swiper-container", {
 
 
 // Header on scroll
+let headerMenu = document.querySelectorAll('header li')
+const HeaderScroll = (e) => {
+    e.target.classList.add('active')
+    headerMenu.forEach(menu => {
+        if (e.target !== menu) {
+            menu.classList.remove('active')
+        }
+    })
+    gsap.to(window, {
+        duration: 1.5,
+        ease: "back.inOut(1.7)",
+        scrollTo: {
+            y: `#${e.target.getAttribute('data-id')}`,
+            offsetY: document.querySelector('header').offsetHeight
+        }
+    });
+}
+headerMenu.forEach(menu => menu.addEventListener('click', HeaderScroll))
